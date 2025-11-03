1892b3b52abf128948c87cbe9e0b1cf3abdf9202
subodhvermaofficial <subodh.verma.official@gmail.com>
2025-11-03 11:38:29 -0500

    modified security group id

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   protocol              = "tcp"
   cidr_blocks           = ["0.0.0.0/0"]
 
-  aws_security_group_id = aws_security_group.blog.id
+  security_group_id     = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_https_in" {
@@ -55,7 +55,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   protocol              = "tcp"
   cidr_blocks           = ["0.0.0.0/0"]
 
-  aws_security_group_id = aws_security_group.blog.id
+  security_group_id     = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_everything_out" {
@@ -65,5 +65,5 @@ resource "aws_security_group_rule" "blog_everything_out" {
   protocol              = "-1"
   cidr_blocks           = ["0.0.0.0/0"]
 
-  aws_security_group_id = aws_security_group.blog.id
+  security_group_id     = aws_security_group.blog.id
 }
