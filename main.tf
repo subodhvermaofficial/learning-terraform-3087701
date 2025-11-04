f18e579f5d0a5feec3786373419ce5fbab207252
subodhvermaofficial <subodh.verma.official@gmail.com>
2025-11-03 23:24:59 -0500

    fixed egress code

--- main.tf
+++ main.tf
@@ -38,9 +38,12 @@ module "blog_sg" {
 
   vpc_id = data.aws_vpc.default.id
 
-  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
-  egress_rules        = ["all-all"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+
+  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  
 
 }
 
