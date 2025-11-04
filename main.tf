e2ce377ac7ed0400193b4bc9afedf5e7c677c4b0
subodhvermaofficial <subodh.verma.official@gmail.com>
2025-11-04 12:46:19 -0500

    fixed

--- main.tf
+++ main.tf
@@ -61,17 +61,16 @@ module "blog_alb" {
   security_groups = module.blog_sg.security_group_id
   }
 
-   
-  target_groups = {
-    {
-      name_prefix      = "blog-"
-      backend_protocol =  "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id      = aws_instance.blog.id
-          port           = 80
+ target_groups = {
+   {
+     name_prefix      = "blog-"
+     backend_protocol =  "HTTP"
+     backend_port     = 80
+     target_type      = "instance"
+     targets = {
+       my_target = {
+         target_id      = aws_instance.blog.id
+         port           = 80
         }
       }
     }
