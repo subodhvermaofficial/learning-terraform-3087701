6e7c89d1587c513ce37c1990b3e98617b6cb1df1
subodhvermaofficial <subodh.verma.official@gmail.com>
2025-11-03 19:47:05 -0500

    fixed quotes missing

--- main.tf
+++ main.tf
@@ -35,18 +35,16 @@ module "blog_sg" {
   source         = "terraform-aws-modules/security-group/aws"
   version        = "5.3.1"
   name           = "blog_new"
-  
+
   vpc_id = data.aws_vpc.default.id
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0]
+  ingress_cidr_blocks = ["0.0.0.0/0]"
 
   egress_rules        = ["all-all"]
-  ingress_cidr_blocks = ["0.0.0.0/0]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
 
 }
-
-
 
 resource "aws_security_group" "blog" {
   name        = "blog"
